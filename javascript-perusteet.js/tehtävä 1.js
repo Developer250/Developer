aef3a931b28e559ce0cd44633e407d59647373fe
Jani Kekäläinen <92083120+Developer250@users.noreply.github.com>
2022-09-26 20:08:41 +0300

    26.9.2022
    Hei, tässä ei ole tarkoitus, että tehtävä 2-5 toimisi. Vain tehtää 1. Lähetin erillisinä palautuksina ne muut.

--- javascript-perusteet.js/tehtävä 1.js
+++ javascript-perusteet.js/tehtävä 1.js
@@ -1,25 +1,37 @@
 function yksi()
 {
-  var ar0 = document.getElementById("1_1");
-  var ar1 = document.getElementById("1_2");
-  var ar2 = document.getElementById("1_3");
-  const t = [ar0, ar1, ar2];
+// luetaan html-sivulta id:stä 1_1 arvo
+// luetaan html-sivulta id:stä 1_2 arvo
+// luetaan html-sivulta id:stä 1_3 arvo
+  var ar0 = document.getElementById("1_1").value;
+  var ar1 = document.getElementById("1_2").value;
+  var ar2 = document.getElementById("1_3").value;
   var apu = 0;
-  var tulosta = "Alkup. järjestys = " + t + "<br>";
-  for(var i = 0; i < 2; i++)
-  {
-    for(var x = i + 1; x < 4; i++)
-      {
-        if(t[i]>t[x])
-        {
-          apu = t[i];
-          t[i] = t[x];
-          t[x] = apu;
-        }
-      }
-    }
-  tulosta += "Lop. järjestys = " + t;
-  alert(tulosta);
+  //Tehdään muuttuja nimeltään tulosta, jolle annetaan merkkijono Alkup. järjestys = " + ar0 + ", " + ar1 + ", " + ar2 + "<br> ja lopuksi laitetaan sinne väli//
+  var tulosta = "Alkup. järjestys = " + ar0 + ", " + ar1 + ", " + ar2 + "<br>";
+  //if-komennolla tarkastetaan, että onko muuttuja ar0 suurempi kuin muuttuja ar1 //
+  //Seuraavaksi sijoitetaan muuttuja apu muuttujan ar0 arvo, jota tarvitsemme muuttamaan arvot päinvastaiseksi.
+  //Sitten sijoitetaan muuttuja ar0:een arvo ar1:n arvo. Lopuksi sijoitetaan ar1:een apu-muuttujan ar1:n arvo. //
+  if (ar0 > ar1) {
+    apu = ar0;
+    ar0 = ar1;
+    ar1 = apu;
+  }
+  // Myäs alemmassa kohdassa (if-komennosta eteenpäin) tehdään samalla tavalla kuin edellisessä kohdassa, mutta tarkastetaan onko ar0 suurempi kuin ar2//
+  if (ar0 > ar2) {
+    apu = ar0;
+    ar0 = ar2;
+    ar2 = apu;
+  }
+  // Myös tässä kohdassa tehdään samalla tavalla if-komennosta teenpäin, mutta tarkastetaan onko ar1 suurempi kuin ar2 ja sijoitetaan arvot eli tarvittasessa//
+  if (ar1 > ar2) {
+    apu = ar1;
+    ar1 = ar2;
+    ar2 = apu;
+  }
+  //Tulosta muuttujaan lisätään järjestelty luettelo näkyville selaimeen, kun käyttäjä syöttää numeroita//
+   tulosta += "Lopullinen järjestys = " + ar0 + ", " + ar1 + ", " + ar2;
+   //Tässä tulostetaan tuloste muuttujaan tallennettu html-tiedosto//
   document.getElementById("tulosta").innerHTML = tulosta;
 }
 
