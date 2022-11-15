0ea94449e3901db98155c792bbb0cb1db8e819db
Developer250 <jani.kekalainen@hotmail.com>
2022-11-15 16:13:19 +0200

    kommentti3

--- API/script.js
+++ API/script.js
@@ -4,16 +4,16 @@ const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 
+//Haemme ja saamme JSON-vastauksen. Tämä vastaus on JSON-objekti. Tarvitsemme vitsiavaimen tästä esineestä. Poimimme vitsiavaimen arvon ja näytämme sen p-elementissä.
 let getJoke = () =>  {
   jokeContainer.classList.remove("fade");
   fetch(url)
   .then(data => data.json())
   .then(item => {
-    //paragraphi.itse teksti sisällytetään itse vitsi
-    	jokeContainer.textContent = `${item.joke}`;
+    	jokeContainer.textContent = `${item.joke}`;//Vitsiavain
       jokeContainer.classList.add("fade");
   });
 }
-//Bapille toiminto
+//Napille toiminto
 btn.addEventListener("click", getJoke);
 getJoke();
