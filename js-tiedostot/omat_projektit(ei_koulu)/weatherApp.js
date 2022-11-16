b1a236461a6de6fefc0ba6770bb554cdb7f3df22
Developer250 <jani.kekalainen@hotmail.com>
2022-11-16 13:32:58 +0200

    tic-tac-toe (toimii) ja watherApp (ei toimi)

--- js-tiedostot/omat_projektit(ei_koulu)/weatherApp.js
+++ js-tiedostot/omat_projektit(ei_koulu)/weatherApp.js
@@ -1,20 +1,22 @@
 const api = {
-  key: "a2ffde2ee02758e6b281c73ff5e2240c"
-//  url: `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  key: "a2ffde2ee02758e6b281c73ff5e2240c",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
   if (evt.keyCode == 13) {
-    getResults(searchbox.value);
+    var query = getResults(searchbox.value);
     console.log(searchbox.value);
+    getResults(query);
   }
 }
 
 function getResults(query) {
   fetch(`${api.base}weather?q=${query}&units=metricAPPID=${api.key}`)
   .then(weather => {
+    console.log(weather);
     return weather.json();
   }).then(displayResults);
 }
