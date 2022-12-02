e11dae2f4e49db3292bb72369d2d7d7dcdebb8cf
Developer250 <jani.kekalainen@hotmail.com>
2022-12-02 15:23:23 +0200

    cv ja contact sivustoihin menee klikatessa niitä nappeja

--- js-tiedostot/omat_projektit(ei_koulu)/portfolio.js
+++ js-tiedostot/omat_projektit(ei_koulu)/portfolio.js
@@ -1,3 +1,8 @@
 function portfolio() {
  location.replace("file:///C:/Users/janik/source/repos/Developer/js-tiedostot/omat_projektit(ei_koulu)/portfolio_contact.html")
 }
+
+function cv() {
+  location.replace("https://sway.office.com/xIHKtKwLVdqKZZGg?authoringPlay=true&publish")
+
+}
