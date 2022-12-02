78cd35aa95dd5b841573e58fe0af2dd723f91ba8
Developer250 <jani.kekalainen@hotmail.com>
2022-12-02 12:47:05 +0200

    Koodin lyhennys

--- pos_neg_tehtava1.js
+++ pos_neg_tehtava1.js
@@ -1,16 +1,19 @@
 function pos_neg_tehtava1() {
-  var  t[0];
+  //Määritykset t0 ja t1
+  var = t[0];
   var = t[1];
+  //Verrataan lukuja keskenään
   if (t[1] >= t[0]) {
     console.log("luku on positiivinen")
   }
+  //Verrataan lukuja keskenään
   else if {
     if (t[0] >= t[1])
     console.console.log("luku on negatiivinen");
   }
-  document.getElementById("nappi").onclick = function() {
-    t[1] = document.getElementById("nappi").value;
-    console.console.log(t[1]);
+  document.getElementById("nappi") = function() {
+     document.getElementById("nappi").value;
+
   }
 
 
