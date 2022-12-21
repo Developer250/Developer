35068dc1c62f6bb048b744e2ece39a9752d03ca9
Developer250 <jani.kekalainen@hotmail.com>
2022-12-21 13:04:49 +0200

    Lisää tuotteita ja js tietoja

--- js-tiedostot/omat_projektit(ei_koulu)/grocery_webpage.js
+++ js-tiedostot/omat_projektit(ei_koulu)/grocery_webpage.js
@@ -47,3 +47,23 @@ window.onscroll = () => {
   loginForm.classList.remove('active');
   navbar.classList.remove('active');
 }
+var swiper = new Swiper(".product-slider", {
+  spaceBetween:20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    991:{
+      slidesPerView: 3,
+    },
+  },
+});
