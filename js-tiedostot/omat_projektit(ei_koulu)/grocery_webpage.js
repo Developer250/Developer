87f590ddbc7cd0e1e09b4d556ee6c688d3a769a6
Developer250 <jani.kekalainen@hotmail.com>
2022-12-21 15:51:52 +0200

    Korjauksia

--- js-tiedostot/omat_projektit(ei_koulu)/grocery_webpage.js
+++ js-tiedostot/omat_projektit(ei_koulu)/grocery_webpage.js
@@ -2,7 +2,7 @@
 
 let searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () => {
+  document.querySelector('#search-btn').onclick = () => {
   searchForm.classList.toggle('active');
   shoppingCart.classList.remove('active');
   loginForm.classList.remove('active');
